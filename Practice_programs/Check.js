75bd21cd046117887bcc6351c4594050cc02ea8d
Anurag Tiwari <35424075+anuragt96@users.noreply.github.com>
2025-09-14 00:54:12 +0530

    another one

---
+++ Practice_programs/Check.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+
+let a = Number(prompt("Enter the first number : "));
+let b = Number(prompt("Enter the second number : "));
+
+//checking the datatype of the variables
+
+console.log("Data type of a : " + typeof(a));
+console.log("Data type of b : " + typeof(b));
+
