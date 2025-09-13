7ba533a8de5952e6bd3e011eb24c189b421c5d6e
Anurag Tiwari <35424075+anuragt96@users.noreply.github.com>
2025-09-14 00:51:11 +0530

    another one

---
+++ Practice_programs/Concat.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+let f_name = prompt("Enter the first name : ");
+let l_name = prompt("Enter the last name : ");
+
+ console.log("Concatination of the names are : " + f_name + " " +l_name);
+ 
