3404f5bcd6cdf22089365ee36eb615751c1c1cbf
Anurag Tiwari <35424075+anuragt96@users.noreply.github.com>
2025-09-14 00:38:00 +0530

    another one

---
+++ Practice_programs/Assign.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+
+//assigning the number into the variable using "=" operator which is equal to and typecasting the va
+
+let a = Number(prompt("Enter the first number : "));
+let b = Number(prompt("Enter the second number : "));
+
+let pi = 3.14;
+
+
+console.log(typeof(pi));
+console.log(typeof(a));
+console.log(typeof(b));
+
+
