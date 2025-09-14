3c9cc1d08ebda46c8fb91ccfe013f491a1280a38
Anurag Tiwari <35424075+anuragt96@users.noreply.github.com>
2025-09-14 16:25:30 +0530

    another one

---
+++ mid_level/largest1.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')();
+
+
+let a = Number(prompt("Enter the first number : "));
+let b = Number(prompt("Enter the second number : "));
+let c = Number(prompt("Enter the third number : "));
+
+
+if (a > b && a > c)
+{
+    console.log(`${a} is greater than ${b} & ${c}`);
+}
+else if(b > a && b > c)
+{
+    console.log(`${b} is greater than ${a} & ${c}`);
+}
+else console.log(`${c} is greater than ${a} & ${b}`);
