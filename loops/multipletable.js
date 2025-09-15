da57918fb3753dc8f568ab252d874706593b94c1
Anurag Tiwari <35424075+anuragt96@users.noreply.github.com>
2025-09-15 22:42:25 +0530

    table

---
+++ loops/multipletable.js
@@ -0,0 +1,12 @@
+range = 5;
+let i,j;
+
+
+for (let i = 1; i <=range; i++) 
+{
+    for (let j = 1; j<=10 ;j++)
+    {    
+        console.log( `${i} x ${j}` + " = " + i * j );    
+    }
+    console.log('\n\n');   
+}
