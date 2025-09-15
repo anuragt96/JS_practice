13df892d402e17f82ffed6c502ed3607cd5053de
Anurag Tiwari <35424075+anuragt96@users.noreply.github.com>
2025-09-15 22:36:45 +0530

    changess and another one

--- loops/sumodd.js
+++ loops/sumodd.js
@@ -2,9 +2,9 @@ let l = 100;
 let i, sum = 0;
 for(i = 1 ; i<= l ; i++)
 {
-    if(i % 2 == 0)
+    if(i % 2 !== 0)
     {
         sum = sum + i ;       
     }
 }
-    console.log(`The sum of even no's in 1 to 100 are :  ${sum}`);
+console.log(`The sum of odd no's in 1 to 100 are :  ${sum}`);
