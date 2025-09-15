244873b8daa82a9c9c800a8579221dd144673618
Anurag Tiwari <35424075+anuragt96@users.noreply.github.com>
2025-09-15 22:47:14 +0530

    power

---
+++ loops/power.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+
+let a = Number(prompt("Enter the no : "));
+let power = Number(prompt("Enter the power : "));
+
+
+let final_value = a ** power;
+console.log(final_value);
+
