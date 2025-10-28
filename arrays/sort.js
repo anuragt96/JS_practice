44222518b0ad8fc539c7809d71e46388d2720f09
Anurag Tiwari <35424075+anuragt96@users.noreply.github.com>
2025-10-28 23:20:16 +0530

    another one

--- arrays/sort.js
+++ arrays/sort.js
@@ -3,7 +3,7 @@ const prompt = require('prompt-sync')();
 let arr = [5,1,7,2,9,0];
 
 for (let i = 0; i < arr.length; i++) 
-{
+{ 
     for (let j = 0; j < arr.length - i - 1; j++) 
     {
         if (arr[j] > arr[j + 1]) 
