34713b24841627b51f10a1a2bd93d7e8e90f95f3
Anurag Tiwari <35424075+anuragt96@users.noreply.github.com>
2025-10-28 22:33:57 +0530

    2nd one

---
+++ arrays/sumArray.js
@@ -0,0 +1,12 @@
+
+
+let arr = [1,3,4,6,7,8,9];
+let sum = 0;
+
+//printing the array
+for (let j = 0; j < arr.length ; j++)
+{
+    sum = sum + arr[j];   
+}
+console.log('The sum of an array is : ' + sum);
+
