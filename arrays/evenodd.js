e3ab8960c0b5253092a0f9c4f4dd77bbd92b07c3
Anurag Tiwari <35424075+anuragt96@users.noreply.github.com>
2025-10-28 23:19:55 +0530

    another one

--- arrays/evenodd.js
+++ arrays/evenodd.js
@@ -2,8 +2,6 @@ const prompt = require('prompt-sync')();
 
 let arr = [2,4,6,7,8,9];
 let counter = 0, oddcounter = 0;
-// let a = Number(prompt("Enter the number you want to search in array : "));
-
 
 for (let i = 0 ; i< arr.length;i++)
 {
